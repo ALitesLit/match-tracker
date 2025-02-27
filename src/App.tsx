1f3b504218c29bed2ffa7d301eec0411c62d8865
Alite <saha290808@gmail.com>
2025-02-27 17:39:38 +0300

    Inital commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import Main from './pages/Main/Main';
 
